13849fc72c175fd9eefc7d8df7cac766818a03e5
globalsys.igor <globalsys.igor@meupag.com.br>
2019-12-27 18:21:45 -0300

    repository implementation started

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:trivia_number/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia{
+  NumberTriviaModel({
+    @required String text,
+    @required int number
+  }) : super(text: text, number: number);
+
+ factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+  return NumberTriviaModel(
+    text: json['text'],
+    // The 'num' type can be both a 'double' and an 'int'
+    number: (json['number'] as num).toInt(),
+  );
+}
+ 
+}
