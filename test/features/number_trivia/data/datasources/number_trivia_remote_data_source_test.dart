178ce1edfaa66f4f4015be206ed56948ded020b7
globalsys.igor <globalsys.igor@meupag.com.br>
2019-12-31 11:52:14 -0300

    getConcreteNumberTrivia tests for success and failure completed

---
+++ test/features/number_trivia/data/datasources/number_trivia_remote_data_source_test.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:trivia_number/core/error/exceptions.dart';
+import 'package:trivia_number/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:trivia_number/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:matcher/matcher.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+void main() {
+  NumberTriviaRemoteDataSourceImpl dataSource;
+  MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    dataSource = NumberTriviaRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  void setUpMockHttpClientSuccess200() {
+    when(mockHttpClient.get(any, headers: anyNamed('headers')))
+      .thenAnswer((_) async => http.Response(fixture('trivia.json'), 200));
+  }
+
+  void setUpMockHttpClientFailure404() {
+      when(mockHttpClient.get(any, headers: anyNamed('headers')))
+      .thenAnswer((_) async => http.Response('Something went wrong', 404));
+  }
+
+  group('getConcreteNumberTrivia', () {
+    final tNumber = 1;
+    final tNumberTriviaModel = NumberTriviaModel.fromJson(json.decode(fixture('trivia.json')));
+
+    test('''perform a GET request on a URL with number
+    being the endpoint and with application/json header''', 
+    () async {
+      // arrange
+      setUpMockHttpClientSuccess200();
+      // act
+      dataSource.getConcreteNumberTrivia(tNumber);
+      // assert
+      verify(mockHttpClient.get(
+        'http://numbersapi.com/$tNumber',
+        headers:{'Content-Type': 'application/json'}
+      ));
+    });
+
+    test('return NumberTrivia when the response code is 200 (success)',
+    () async {
+      // arrange
+      setUpMockHttpClientSuccess200();
+      // act
+      final result  = await dataSource.getConcreteNumberTrivia(tNumber);
+      // assert
+      expect(result, equals(tNumberTriviaModel));
+    });
+
+    test('throw a ServerException when the response is 404 or other',
+    () async {
+      // arrange
+      setUpMockHttpClientFailure404();
+      // act
+      final call  = dataSource.getConcreteNumberTrivia;
+      // assert
+      expect(() => call(tNumber), throwsA(TypeMatcher<ServerException>()));
+    });
+  });
+}
